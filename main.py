08c688ba6455be1574f792ca3241e654b9687475
mefema1847 <mefema1847@helesco.com>
2026-02-23 12:36:46 +0530

    Update main.py changes

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from telethon.sync import TelegramClient
 
 
 # === CONFIGURATION ===
-# api_id = int(os.getenv("API_ID"))
-# api_hash = os.getenv("API_HASH")
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("BOT_TOKEN")
 
 source_channels = os.getenv("SOURCE_CHANNELS").split(",")
